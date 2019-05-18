bd15e6bc4d3f9cccf39e66bf510931d15c003733
Alex Conlin-Oakley <alex.conlin-oakley@timgroup.com>
2019-05-18 15:46:48 +0200

    working

--- tcr-java/src/test/java/com/kata/FibonacciTest.java
+++ tcr-java/src/test/java/com/kata/FibonacciTest.java
@@ -5,21 +5,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 @Test
-public class FibonacciShould {
+public class FibonacciTest {
 
-    public void assert0() {
+    public void given0shouldreturn0() {
         assertThat(Fibonacci.fib(0), is(0));
     }
 
-    public void assert1() {
+    public void given1shouldreturn1() {
         assertThat(Fibonacci.fib(1), is(1));
     }
 
-    public void assertInput2() {
+    public void given2shouldreturn1() {
         assertThat(Fibonacci.fib(2), is(1));
     }
 
-    public void assertInput3() {
+    public void given3shouldreturn2() {
         assertThat(Fibonacci.fib(3), is(2));
     }
 }
